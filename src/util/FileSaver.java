723d45bab44c00673e43823ef4be6590708fe5c7
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2022-02-11 14:54:42 +0100

    add static block to create folder if it doesn't exist

--- src/util/FileSaver.java
+++ src/util/FileSaver.java
@@ -3,6 +3,8 @@ package util;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Map;
 
 import org.json.simple.JSONArray;
@@ -10,7 +12,15 @@ import org.json.simple.JSONArray;
 public class FileSaver {
 
 	public static final String FOLDER_PATH = "files/";
-
+	
+	static {
+		try {
+			Files.createDirectory(Paths.get(FOLDER_PATH));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public static void saveDataAsJSON(Map<String,JSONArray> data) {
 		data.forEach((table, rows)->{
 			try (BufferedWriter writer = new BufferedWriter(new FileWriter(FOLDER_PATH + table+".json"))) {
