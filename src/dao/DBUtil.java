3e639e5fa44f96efc3f61243da27dee7e53af444
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2022-02-11 14:45:58 +0100

    add getRelatedTablesData and getRelatedTablesNames methods

--- src/dao/DBUtil.java
+++ src/dao/DBUtil.java
@@ -6,7 +6,11 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -45,21 +49,43 @@ public class DBUtil {
 		return relationships;
 	}
 
-	@SuppressWarnings("unchecked")
-	public static JSONArray getTableData(String tableName) {
-		JSONArray data = new JSONArray();
-		try (ResultSet resultSet = DBConnection.getConnection().createStatement()
-				.executeQuery("SELECT * FROM " + tableName)) {
-			ResultSetMetaData metadata = resultSet.getMetaData();
-			while (resultSet.next()) {
-				JSONObject json = new JSONObject();
-				for (int i = 1; i <= metadata.getColumnCount(); i++)
-					json.put(metadata.getColumnName(i), resultSet.getString(i));
-				data.add(json);
+	private static Set<String> getRelatedTablesNames(String dbName) {
+		Set<String> names = new HashSet<>();
+		try {
+			Connection connection = DBConnection.getConnection();
+			PreparedStatement ps = connection.prepareStatement("SELECT `TABLE_NAME`, `REFERENCED_TABLE_NAME`"
+					+ "FROM `INFORMATION_SCHEMA`.`KEY_COLUMN_USAGE` "
+					+ "WHERE `TABLE_SCHEMA` = ? AND `REFERENCED_TABLE_NAME` IS NOT NULL " + "ORDER BY `TABLE_NAME`");
+			ps.setString(1, dbName);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				names.add(rs.getString("TABLE_NAME"));
+				names.add(rs.getString("REFERENCED_TABLE_NAME"));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		return names;
+	}
+
+	@SuppressWarnings("unchecked")
+	public static Map<String, JSONArray> getRelatedTablesData(String dbName) {
+		Map<String, JSONArray> data = new TreeMap<>();
+		for (String tableName : getRelatedTablesNames(dbName))
+			try (ResultSet resultSet = DBConnection.getConnection()
+					.prepareStatement("SELECT * FROM `" + dbName + "`.`" + tableName + "`").executeQuery()) {
+				ResultSetMetaData metadata = resultSet.getMetaData();
+				JSONArray rows = new JSONArray();
+				while (resultSet.next()) {
+					JSONObject row = new JSONObject();
+					for (int i = 1; i <= metadata.getColumnCount(); i++)
+						row.put(metadata.getColumnName(i), resultSet.getString(i));
+					rows.add(row);
+				}
+				data.put(tableName, rows);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		return data;
 	}
 
