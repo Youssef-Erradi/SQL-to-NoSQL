cd18a017df663b007c73a16f8a89e049c69a67b3
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2022-02-15 20:45:09 +0100

    add relationship class

---
+++ src/beans/Relationship.java
@@ -0,0 +1,57 @@
+package beans;
+
+import java.io.Serializable;
+
+public class Relationship implements Serializable {
+	private static final long serialVersionUID = -6426616525486873004L;
+	private String tableName, columnName, referencedTableName, referencedColumnName;
+
+	public Relationship() {
+	}
+
+	public Relationship(String tableName, String columnName, String referencedTableName, String referencedColumnName) {
+		this.tableName = tableName;
+		this.columnName = columnName;
+		this.referencedTableName = referencedTableName;
+		this.referencedColumnName = referencedColumnName;
+	}
+
+	public String getTableName() {
+		return tableName;
+	}
+
+	public void setTableName(String tableName) {
+		this.tableName = tableName;
+	}
+
+	public String getColumnName() {
+		return columnName;
+	}
+
+	public void setColumnName(String columnName) {
+		this.columnName = columnName;
+	}
+
+	public String getReferencedTableName() {
+		return referencedTableName;
+	}
+
+	public void setReferencedTableName(String referencedTableName) {
+		this.referencedTableName = referencedTableName;
+	}
+
+	public String getReferencedColumnName() {
+		return referencedColumnName;
+	}
+
+	public void setReferencedColumnName(String referencedColumnName) {
+		this.referencedColumnName = referencedColumnName;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("`%s` de `%s` fait référence à `%s` de `%s`", columnName, tableName, referencedColumnName,
+				referencedTableName);
+	}
+
+}
