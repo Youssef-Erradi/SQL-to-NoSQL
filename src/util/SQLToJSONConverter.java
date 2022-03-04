0fe6823b957f3937439ac8671c7999435ad3cc09
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2022-03-04 20:03:50 +0100

    fix bugs in statementsSplit method

--- src/util/SQLToJSONConverter.java
+++ src/util/SQLToJSONConverter.java
@@ -5,53 +5,90 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@SuppressWarnings({"rawtypes", "unchecked"})
 public class SQLToJSONConverter {
 
-	private List<String> getSQLfromFile(String filename) throws IOException {
+	private static List<String> getSQLfromFile(String filename) throws IOException {
 		String content = new String(Files.readAllBytes(new File("files/sql/" + filename).toPath()));
 		String[] sqlStatements = content.split("--\n");
 		return Arrays.asList(sqlStatements);
 	}
-
-	private Map<String, List<String>> splitStatements(String filename) throws IOException {
+	
+	private static List<List<String>> statementsSplit(String filename) throws IOException {
 		System.out.println("Get sql statements from file ....");
-		Map<String, List<String>> infos = new HashMap<>(4);
-		for (String key : new String[] { "data", "tables", "foreign_key", "primary_key" })
-			infos.put(key, new ArrayList<>());
+		List data = new ArrayList<>(),
+					tables= new ArrayList<>(),
+					foreign_key = new ArrayList<>(),
+					primary_key = new ArrayList<>();
 		
 		for(String statement : getSQLfromFile(filename)) {
 			if(statement.contains("INSERT")) {
 				statement = statement.replaceAll("\n", "");
 				statement = statement.replaceAll("----[^--]+--","");
-				infos.get("data").add(statement);
+				data.add(statement);
 				
 				statement = statement.split(" ")[2].replaceAll("`", "");
-				infos.get("tables").add(statement);
+				tables.add(statement);
 				continue;
 			}
 			
 			if(statement.contains("FOREIGN KEY")) {
 				statement = statement.replaceAll("\n", "");
 				statement = statement.replaceAll("----[^--]+--","");
-				Matcher m = Pattern.compile("your regular expression here").matcher(statement);
+				Matcher m = Pattern.compile("`.*?`").matcher(statement);
+				List<String> r = new ArrayList<>();
 				while (m.find()) 
-					infos.get("foreign_key").add(m.group().replaceAll("`", ""));
+					r.add(m.group().replaceAll("`", ""));
+				foreign_key.add(r);
 				continue;
 			}
 			
 			if(statement.contains("PRIMARY KEY")) {
 				statement = statement.replaceAll("\n", "");
 				statement = statement.replaceAll("----[^--]+--","");
-				infos.get("primary_key").add(statement);
+				primary_key.add(statement);
 				continue;
 			}
 		}
-		return infos;
+		String temp = String.join("  ", data);
+		data.clear();
+		List<String> dat = Arrays.asList( temp.split("\\);") );
+		dat = dat.subList(0, dat.size());
+
+		for(String r : dat) {
+			r = r.replaceAll("^-- ------------------------------------------------------  ", "");
+			data.add( String.join(" ", r, ");") );
+		}
+		
+		List primaryKeyData = new ArrayList<>();
+		for(Object obj : primary_key) {
+			String key = (String) obj;
+			List<String> k = new ArrayList<>();
+			Matcher m = Pattern.compile("\\`.*?\\`").matcher(key);
+			while (m.find()) 
+				k.add(m.group().replaceAll("`", ""));
+			primaryKeyData.add(k.subList(0, 2));
+		}
+		
+		List foreign = new ArrayList<>();
+		for(Object obj : foreign_key) {
+			List<String> k = (List<String>) obj;
+			if( tables.contains(k.get(0)) ) {
+				foreign.add(k);
+			}
+		}
+		
+		List info = new ArrayList<>();
+		info.add(tables);
+		info.add(data);
+		info.add(foreign);
+		info.add(primaryKeyData);
+		
+		return info;
 	}
+
 }
